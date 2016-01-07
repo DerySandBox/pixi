6f86d4b267bc574d51d2a61de1a2e9fb4f0501b2
Dery <>
2016-01-06 23:18:24 -0500

    just test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body><h1>Taste on Pixi</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
